c122238711e928b6d4a88026cfaa4549ebd6ddad
sandeepsachdev <sandeepsachdev17@gmail.com>
2026-04-15 20:34:08 +1000

    Fix quota display

--- src/main/java/com/watchmode/api/model/StatusResponse.java
+++ src/main/java/com/watchmode/api/model/StatusResponse.java
@@ -1,8 +1,6 @@
 package com.watchmode.api.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public record StatusResponse(
-        @JsonProperty("account_quota") int quota,
-        @JsonProperty("quota_used") int quotaUsed
+        int quota,
+        int quotaUsed
 ) {}
